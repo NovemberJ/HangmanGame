c57a41e64af53ae9cce3ed7faa1f2f058a2b14c1
NovemberJ <iuliiaizmailova22@gmail.com>
2025-10-26 12:43:29 +0200

    Set dark mode switcher

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import '../css/style.css'
+
+const darkModeSwitcher = document.getElementById('toggleDarkMode')
+darkModeSwitcher.addEventListener('input', () => {
+    const htmlElement = document.documentElement
+    htmlElement.classList.toggle('dark')
+})
