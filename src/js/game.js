0242c61c79dbc6d4aa0da0f7efaf3a786031d9fd
NovemberJ <iuliiaizmailova22@gmail.com>
2025-11-11 20:54:17 +0200

    Modified placeholders for the letters

--- src/js/game.js
+++ src/js/game.js
@@ -1,8 +1,26 @@
 import {WORDS} from "./consts.js";
 
+const gameDiv= document.getElementById("game")
+
+const createPlaceHolderHTML = () => {
+    const word = sessionStorage.getItem('word')
+    let placeholderHTML = ''
+    for (let i = 0; i < word.length; i++) {
+        placeholderHTML = placeholderHTML + `<h1 id="letter_${i}" class="letter">_</h1>`
+    }
+
+    return `<div id="placeholders" class="placeholders-wrapper">${placeholderHTML}</div>`
+}
+
 export const startGame = () => {
     const randomIndex = Math.floor(Math.random() * WORDS.length);
     const wordToGuess = WORDS[randomIndex]
     sessionStorage.setItem("word", wordToGuess);
 
+    let placeholderHTML = ''
+    for (let i = 0; i < wordToGuess.length; i++) {
+        placeholderHTML = placeholderHTML + `<h1 id="letter_${i}" class="letter">_</h1>`
+    }
+
+    gameDiv.innerHTML = createPlaceHolderHTML()
 }
