6350f3b1a5496a4621f002aa6eed28cb87f9a9de
NovemberJ <iuliiaizmailova22@gmail.com>
2025-11-26 13:56:57 +0200

    Added hangman image and number of tries

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import {WORDS, KEYBOARD_LETTERS} from "./consts.js";
 
 const gameDiv= document.getElementById("game")
+const logoH1 = document.createElement("logo");
 
 const createPlaceHolderHTML = () => {
     const word = sessionStorage.getItem('word');
@@ -16,19 +17,39 @@ const createKeyboard = () => {
     keyboard.id = 'keyboard'
 
     const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, curr) => {
-        return acc + `<button class="button-primary" id="${curr}">${curr}</button>`
+        return acc + `<button class="button-primary keyboard-button" id="${curr}">${curr}</button>`
     }, '')
 
     keyboard.innerHTML = keyboardHTML;
     return keyboard
 }
 
+    const createHangmanImg = () => {
+        const image = document.createElement("img");
+        image.src = 'images/hg-5.png';
+        image.alt = 'hangman image';
+        image.classList.add('hangman-img');
+        image.id = 'hangman-img';
+        return image;
+    }
+
 export const startGame = () => {
+    logoH1.classList.add('logo-sm')
     const randomIndex = Math.floor(Math.random() * WORDS.length);
     const wordToGuess = WORDS[randomIndex]
     sessionStorage.setItem("word", wordToGuess);
 
     gameDiv.innerHTML = createPlaceHolderHTML()
+
+    gameDiv.innerHTML += '<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">10</span></p>'
+
     const keyboardDiv = createKeyboard()
+    keyboardDiv.addEventListener('click', (event) => {
+        console.log(event.target.id);
+    });
+
+    const hangmanImg = createHangmanImg();
+    gameDiv.prepend(hangmanImg);
+
     gameDiv.appendChild(keyboardDiv)
 }
